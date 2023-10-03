1aeb40ad6fa026ee7febd677e21f0c6b7bb9b974
Vinit Churi <vinitchuri@hotmail.com>
2023-10-03 08:04:33 +0530

    tailwind and basic style setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <div className="h1-bold">
+      <h1>started</h1>
+    </div>
+  );
 }
