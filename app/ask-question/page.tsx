c46a775080705eedd81684f78e68ce9fb1ccd41d
Vinit Churi <vinitchuri@hotmail.com>
2023-10-05 14:40:42 +0530

    auth

---
+++ app/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>protected ask question</div>;
+};
+
+export default page;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="h1-bold">
-      <h1>started</h1>
-    </div>
-  );
-}
