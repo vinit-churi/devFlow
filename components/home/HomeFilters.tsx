5cadfe44105765bf4b6a97de323871299d0ebb6b
Vinit Churi <vinitchuri@hotmail.com>
2023-10-08 11:15:08 +0530

    add home filter

---
+++ components/home/HomeFilters.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { HomePageFilters } from "@/constants/filters";
+import React from "react";
+import { Button } from "@/components/ui/button";
+const HomeFilters = () => {
+  const active = "newest";
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {HomePageFilters.map((filter) => (
+        <Button
+          key={filter.value}
+          className={`body-medium rounded-md px-6 py-3 capitalize shadow-none ${
+            active === filter.value
+              ? "bg-primary-100 text-primary-500"
+              : "bg-light-800 text-light-500 "
+          }`}
+        >
+          {filter.value}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default HomeFilters;
---
+++ components/shared/Filter.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface IFilterProps {
+  filters: {
+    name: string;
+    value: string;
+  }[];
+  otherClasses?: string;
+  containerClasses?: string;
+}
+
+const Filter = ({ filters, otherClasses, containerClasses }: IFilterProps) => {
+  return (
+    <div className={`relative ${containerClasses}`}>
+      <Select>
+        <SelectTrigger
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
+        >
+          <div className="line-clamp-1 flex-1 text-left">
+            <SelectValue placeholder="Select a Filter" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {filters.map((filter, index) => (
+              <SelectItem
+                key={index}
+                className="body-regular text-dark500_light700"
+                value={filter.value}
+              >
+                {filter.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default Filter;
