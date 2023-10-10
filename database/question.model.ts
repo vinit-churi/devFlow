aada864639a383392ac0d494fd6d0ad6f2a01b42
Vinit Churi <vinitchuri@hotmail.com>
2023-10-10 06:52:06 +0530

    add question modal

---
+++ database/question.model.ts
@@ -0,0 +1,50 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IQuestion extends Document {
+    title: string;
+    content: string;
+    tags: Schema.Types.ObjectId[];
+    upvotes: Schema.Types.ObjectId[];
+    downvotes: Schema.Types.ObjectId[];
+    author : Schema.Types.ObjectId;
+    answers: Schema.Types.ObjectId[];
+    createdAt: Date;
+}
+
+const QuestionSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+    },
+    tags: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Tag',
+    }],
+    upvotes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    downvotes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    author : {
+        type: Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    answers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Answer',
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Question = models.Question || model<IQuestion>('Question', QuestionSchema);
+
+export default Question;
