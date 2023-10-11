3eda3f97d6573c6079f853f5d027a6f27000078a
Vinit Churi <vinitchuri@hotmail.com>
2023-10-11 06:48:20 +0530

    setup webhook

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,80 @@
+/* eslint-disable camelcase */
+import type { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { WebhookRequiredHeaders } from "svix";
+import type { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+
+type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
+
+// TODO: Add your webhook secret to your environment variables
+const webhookSecret: string | undefined = process.env.WEBHOOK_SECRET;
+
+export default async function handler(
+  req: NextApiRequestWithSvixRequiredHeaders,
+  res: NextApiResponse
+) {
+  const payload = JSON.stringify(req.body);
+  const headers = req.headers;
+  // Create a new Webhook instance with your webhook secret
+  if (!webhookSecret) throw new Error("Webhook secret not found");
+  const wh = new Webhook(webhookSecret);
+
+  let evt: WebhookEvent;
+  try {
+    // Verify the webhook payload and headers
+    evt = wh.verify(payload, headers) as WebhookEvent;
+  } catch (_) {
+    // If the verification fails, return a 400 error
+    return res.status(400).json({});
+  }
+  // const { id } = evt.data;
+
+  const eventType = evt.type;
+  if (eventType === "user.created") {
+    const { email_addresses, image_url, username, first_name, last_name, id } =
+      evt.data;
+    console.log(`User ${id} was ${eventType}`);
+    const mangoUser = createUser({
+      clerkId: id,
+      name: `${first_name} ${last_name || ""}`,
+      username: username as string,
+      email: email_addresses[0].email_address,
+      picture: image_url,
+    });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: mangoUser });
+  }
+  if (eventType === "user.updated") {
+    const { email_addresses, image_url, username, first_name, last_name, id } =
+      evt.data;
+    console.log(`User ${id} was ${eventType}`);
+
+    const mangoUser = updateUser({
+      clerkId: id,
+      updateData: {
+        name: `${first_name} ${last_name || ""}`,
+        username: username as string,
+        email: email_addresses[0].email_address,
+        picture: image_url,
+      },
+      path: `/profile/${id}`,
+    });
+    res
+      .status(201)
+      .json({ message: "User updated successfully", user: mangoUser });
+  }
+
+  if (eventType === "user.deleted") {
+    const { id } = evt.data;
+    console.log(`User ${id} was ${eventType}`);
+    const deletedUser = await deleteUser(id!);
+    res
+      .status(200)
+      .json({ message: "User deleted successfully", user: deletedUser });
+  }
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,17 +1,62 @@
-"use server"
+"use server";
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
+export async function getUserById(params: any) {
+  try {
+    await connectToDatabase();
+    const { userId } = params;
+    const user = await User.findOne({ clerkId: userId });
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
-export async function getUserById(params: any){
-    try{
-        await connectToDatabase();
-        const { userId } = params;
-        const user = await User.findOne({clerkId : userId});
-        return user;
-    }catch (error){
-        console.log(error);
-        throw error;
-    }
+export async function createUser(userData: CreateUserParams) {
+  try {
+    await connectToDatabase();
+    const user = await User.create(userData);
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(userData: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const { clerkId, updateData, path } = userData;
+    const user = await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function deleteUser(id: string) {
+  try {
+    await connectToDatabase();
+    const user = await User.findOne({ clerkId: id });
+    if (!user) throw new Error("User not found");
+    // delete everything related to the user
+    // const userQuestionIds = await Question.find({ "author": user._id }).distinct("_id");
+    // delete all questions
+    await Question.deleteMany({ author: user._id });
+    const deletedUser = await User.findOneAndDelete({ clerkId: id });
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
