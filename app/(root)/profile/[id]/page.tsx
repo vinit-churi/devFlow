5eca495b564a95b340acc6dcbe20770da3effa95
Vinit Churi <vinitchuri@hotmail.com>
2023-10-16 16:38:56 +0530

    done with the challenge

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -69,7 +69,10 @@ const page = async ({ params, searchParams }: URLProps) => {
           </SignedIn>
         </div>
       </div>
-      <Stats />
+      <Stats
+        totalQuestions={userInfo.totalQuestions}
+        totalAnswers={userInfo.totalAnswers}
+      />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
           <TabsList className="background-light800_dark400 min-h-[42px] p-1">
--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,5 +1,60 @@
-const Stats = () => {
-  return <div>Stats</div>;
+import Image from "next/image";
+
+interface IProps {
+  totalQuestions: number;
+  totalAnswers: number;
+}
+const Stats = ({ totalQuestions, totalAnswers }: IProps) => {
+  return (
+    <div className=" grid w-full grid-cols-1 gap-6 md:grid-cols-2  lg:grid-cols-4">
+      <div className="flex flex-wrap items-center justify-center gap-4 rounded-lg p-5 shadow-md">
+        <div>
+          <p>{totalQuestions}</p>
+          <p className="text-sm">Questions</p>
+        </div>
+        <div>
+          <p>{totalAnswers}</p>
+          <p className="text-sm">Answers</p>
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-4 rounded-lg p-5 shadow-md">
+        <Image
+          src="/assets/icons/gold-medal.svg"
+          alt="answer"
+          width={32}
+          height={32}
+        />
+        <div className="w-auto">
+          <p className="text-sm">0</p>
+          <p className="text-sm">Gold Badges</p>
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-4 rounded-lg p-5 shadow-md">
+        <Image
+          src="/assets/icons/silver-medal.svg"
+          alt="answer"
+          width={24}
+          height={24}
+        />
+        <div>
+          <p className="text-sm">0</p>
+          <p className="text-sm">Silver Badges</p>
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-4 rounded-lg p-5 shadow-md">
+        <Image
+          src="/assets/icons/bronze-medal.svg"
+          alt="answer"
+          width={24}
+          height={24}
+        />
+        <div>
+          <p className="text-sm">0</p>
+          <p className="text-sm">Bronze Badges</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
