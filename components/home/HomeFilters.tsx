c3c50db06af16aca6d3d6a5ddfdcbcec6f615c7a
Vinit Churi <vinitchuri@hotmail.com>
2023-11-13 10:04:00 +0530

    Add filter functionality to HomeFilters component

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,14 +1,39 @@
 "use client";
 import { HomePageFilters } from "@/constants/filters";
-import React from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 const HomeFilters = () => {
-  const active = "newest";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentFilter = searchParams.get("filter");
+  const [active, setActive] = useState(currentFilter || "");
+  function handleClick(item: string) {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  }
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((filter) => (
         <Button
           key={filter.value}
+          onClick={() => handleClick(filter.value)}
           className={`body-medium rounded-md px-6 py-3 capitalize shadow-none ${
             active === filter.value
               ? "bg-primary-100 text-primary-500"
