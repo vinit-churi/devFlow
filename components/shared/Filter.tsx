f3ff56de6ed6fa82d07bd6c08a2495dd62943e82
Vinit Churi <vinitchuri@hotmail.com>
2023-11-14 08:35:41 +0530

    Add default value to Select component in Filter.tsx

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -47,7 +47,7 @@ const Filter = ({ filters, otherClasses, containerClasses }: IFilterProps) => {
   }
   return (
     <div className={`relative ${containerClasses}`}>
-      <Select onValueChange={handleChange}>
+      <Select defaultValue={currentFilter || ""} onValueChange={handleChange}>
         <SelectTrigger
           className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
